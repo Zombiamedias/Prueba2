efe748db89a9a43d5ca1a51cf1c3851f86e37451
MapeMC <michaellipe@hotmail.com>
2025-01-07 21:04:55 -0500

    pagination logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
-import './App.css'
-import SearchBar from './components/searchBar';
+import "./App.css";
+import React, { Component } from "react";
+import SearchBar from "./components/searchBar";
 import Table from "./components/table.jsx";
-function App() {
-  return (
-    <>
-      <SearchBar/>
-      <Table/>
-    </>
-  )
+import Pagination from "./components/pagination.jsx";
+import Item from "./components/item";
+class App extends Component {
+  state = {
+    allSongs: [],
+    currentSongs: [],
+    currentPage: null,
+    totalPags: null,
+  };
+  componentDidMount() {
+    const { data: allSongs = [] } = Songs.findAll();
+    this.setState({ allSongs });
+  }
+  onPageChanged = (data) => {
+    const { allSongs } = this.state;
+    const { currentPage, totalPags, pagLimit } = data;
+    const offset = (currentPage - 1) * pagLimit;
+    const currentSongs = allSongs.slice(offset, offset + pagLimit);
+    this.setState({ currentPage, currentSongs, totalPags });
+  };
 }
-
-export default App
+export default App;
--- src/components/pagination.jsx
+++ src/components/pagination.jsx
@@ -1,29 +1,173 @@
 import PropTypes from "prop-types";
 import React, { Component, Fragment } from "react";
 
+// RANGE SLIDER
+const leftP = "LEFT";
+const rightP = "RIGHT";
+
+const range = (from, to, step = 1) => {
+  var i = from;
+  const range = [];
+  //logic
+  while (i <= to) {
+    range.push(i);
+    i += step;
+  }
+  return range;
+};
 
 // pagination component
 class Pagination extends Component {
+  // pages constructor
   constructor(props) {
     super(props);
-    const { totalSongs = null, pageLimit = 30, pageNeighbours = 0 } = props; //props delcaration 
-    this.pageLimit = typeof pageLimit === "number" ? pageLimit : 30; // pagelimit is set to 30 
+    const { totalSongs = null, pagLimit = 30, pagNeighbours = 0 } = props; //props delcaration
+    this.pagLimit = typeof pagLimit === "number" ? pagLimit : 30; // pagelimit is set to 30
     this.totalSongs = typeof totalSongs === "number" ? totalSongs : 0; // total songs is set to 0
 
-    this.pageNeighbours =
-      typeof pageNeighbours === "number"
-        ? Math.max(0, Math.min(pageNeighbours, 2))
+    this.pagNeighbours =
+      typeof pagNeighbours === "number"
+        ? Math.max(0, Math.min(pagNeighbours, 2))
         : 0; // page neighbours is set to 0 but depends about the results
     this.totalSongs = Math.ceil(this.totalSongs / this.pageLimit); // total songs is calculated by dividing total songs by page limit and rounding up to the nearest integer
-    this.state = { currentpage: 1 }; // current page is set to 1
+    this.state = { crrntpage: 1 }; // current page is set to 1
   }
 }
-// Proptypes
+
+  // pagination methods
+  fetchPageNumbers = () => {
+    const totalPags = this.totalPags;
+    const currentPag = this.state.crrntpage;
+    const pagNeighbours = this.pagNeighbours;
+    const totalNums = this.pagNeighbours * 2 + 3;
+    const totalBlck = totalNums + 2;
+    // logic
+    if (totalPags > totalBlck) {
+      const startPag = Math.max(2, currentPag - pagNeighbours);
+      const endPag = Math.min(totalPags - 1, currentPag + pagNeighbours);
+      let pags = range(startPag, endPag);
+
+      const leftSpll = startPag > 2;
+      const rightSpll = (endPag < totalPags) - 1;
+      const spillOffset = totalNums - pags.length++;
+      // logic cases
+      switch (true) {
+        case leftSpll && !rightSpll: {
+          const extraPgs = range(startPag - spillOffset, startPag - 1);
+          pags = [leftP, ...extraPgs, ...pags];
+          break;
+        }
+
+        case !leftSpll && rightSpll: {
+          const extraPgs = range(endPag + 1, endPag + spillOffset);
+          pags = [...pags, ...extraPgs, rightP];
+          break;
+        }
+        case leftSpll && rightSpll:
+        default: {
+          pags = [leftP, ...pags, rightP];
+          break;
+        }
+      }
+      return [1, ...pags, totalPags];
+    }
+    return range(1, totalPags);
+  }
+
+  render() {
+    if (!this.totalSongs || this.totalPags === 1) return null;
+    const { currentPag } = this.state;
+    const pags = this.fetchPageNumbers();
+    return (
+      <Fragment>
+        <nav aria-label="Results pagination">
+          <ul className="pagination">
+            {pags.map((pag, index) => {
+              // left case
+              if (pag === leftP)
+                return (
+                  <li key={index} className="page-item">
+                    <a
+                      className="page-link"
+                      href="#"
+                      aria-label="Previous"
+                      onClick={this.handleMoveLeft}
+                    >
+                      <span aria-hidden="true">&laquo;</span>
+                      <span className="sr-only">Provious</span>
+                    </a>
+                  </li>
+                );
+              // right case
+              if (pag === rightP)
+                return (
+                  <li key={index} className="page-item">
+                    <a
+                      className="page-link"
+                      href="#"
+                      aria-label="Next"
+                      onClick={this.handleMoveRight}
+                    >
+                      <span aria-hidden="true"></span>
+                      <span className="sr-only">Next</span>
+                    </a>
+                  </li>
+                );
+              // default case
+              return (
+                <li
+                  key={index}
+                  className={`page-items${currentPag === pag ? " active" : ""}`}
+                >
+                  <a
+                    className="page-link"
+                    href="#"
+                    onClick={this.handleClick(pag)}
+                  >
+                    {pag}
+                  </a>
+                </li>
+              );
+
+            } ) }
+
+          </ul>
+        </nav>
+      </Fragment>
+    );
+  }
+
+  //componen Mount
+  componentDidMount() {this.gotoPag(1)}
+
+  gotoPag = (pag) => {
+    const { onPageChanged = (f) => f } = this.props;
+    const currentPag = Math.max(0, Math.min(pag, this.totalPags));
+    const pagData = {
+      currentPag,
+      totalPags: this.pageLimit,
+      totalSongs: this.totalSongs,
+    };
+    this.setState({ currentPag }, () => onPageChanged(pagData));
+
+    handleClick = (pag) => (evt) => {
+      evt.preventDefault();
+      this.gotoPag(pag);
+    };
+    handleMoveLeft = (evt) => {
+      evt.preventDefault();
+      this.gotoPag(this.state.currentPag - this.pagNeighbours * 2 - 1);
+    };
+    handleMoveRight = (evt) => {
+      evt.preventDefault();
+      this.gotoPag(this.state.currentPag + this.pagNeighbours * 2 + 1);
+    };
+    // Proptypes
+}
+
 Pagination.propTypes = {
   totalSongs: PropTypes.number.isRequired,
-  pageLimit: PropTypes.number.isRequired,
-  pageNeighbours: PropTypes.number.isRequired,
+  pagLimit: PropTypes.number.isRequired,
+  pagNeighbours: PropTypes.number.isRequired,
   onPageChanged: PropTypes.func,
 };
-
-export default Pagination;
